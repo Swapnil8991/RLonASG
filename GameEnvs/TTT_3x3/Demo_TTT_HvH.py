af6d6a69859ffec028f1a5aa2f68696ae737b875
NicTeslaJr <nishilpatel112@gmail.com>
2019-12-08 13:44:43 +0530

    Removed unnecessary if condt. from Demo_TTT_HvH.py, and added Requirements in the README

--- GameEnvs/TTT_3x3/Demo_TTT_HvH.py
+++ GameEnvs/TTT_3x3/Demo_TTT_HvH.py
@@ -7,25 +7,22 @@ if __name__ == '__main__':
     b = TTTBoard()
     b.printInfo()
 
-    while True:
-        if b.board[0] < 10:
-            if b.board[0] > 4:
-                status, wSindex = b.winnerCheck()
-                if status == 0:
-                    print("Game Draw!\n")
-                    break
-                elif status == 1:
-                    print(f"Player X Wins! (wState[{wSindex}]: {b.wState[wSindex]})\n")
-                    break
-                elif status == 2:
-                    print(f"Player O Wins! (wState[{wSindex}]: {b.wState[wSindex]})\n")
-                    break
-            cPChar = next(playerCharToggler)
-            cPNum = next(playerNumToggler)
+    while b.board[0] < 10:
+        if b.board[0] > 4:
+            status, wSindex = b.winnerCheck()
+            if status == 0:
+                print("Game Draw!\n")
+                break
+            elif status == 1:
+                print(f"Player X Wins! (wState[{wSindex}]: {b.wState[wSindex]})\n")
+                break
+            elif status == 2:
+                print(f"Player O Wins! (wState[{wSindex}]: {b.wState[wSindex]})\n")
+                break
+        cPChar = next(playerCharToggler)
+        cPNum = next(playerNumToggler)
+        print(f"\nPlayer {cPChar}: ", end='', flush=True)
+        while not b.makeMove(cPNum, int(input())):
+            print("Already Occuipied or Invalid Position", end='')
             print(f"\nPlayer {cPChar}: ", end='', flush=True)
-            while not b.makeMove(cPNum, int(input())):
-                print("Already Occuipied or Invalid Position", end='')
-                print(f"\nPlayer {cPChar}: ", end='', flush=True)
-            b.printBoard()
-        else:
-            break
+        b.printBoard()
--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 Reinforcement Learning on Abstract Strategy Games
 
+Requirements:
+    Python >= 3.6
+
 GTU Final Year Project
 Nishil Patel,  Deep Patel & Swapnil Jagtap
